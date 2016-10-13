cce14ccb02542d0811a3bf0b5a6275c6b6e95ded
soee1-16 <soee1_16@uni.worc.ac.uk>
2016-10-13 14:45:23 +0100

    Change order of functions

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,7 @@
-var paused_count =0;
+var paused_count = 0;
 var resumed_count = 0;
 var launched_count = 0;
 
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
@@ -14,17 +9,6 @@ var launched_count = 0;
 	}
 
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
-        
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
 
     // Handle the pause event
     //
@@ -39,3 +23,25 @@ var launched_count = 0;
 		resumed_count++;
 		updateDisplay();
     }
+
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+		alert("device ready");
+        
+		document.addEventListener("resume", onResume, false);
+		document.addEventListener("pause", onPause, false);
+		
+		launched_count++;
+		updateDisplay();
+    }
+
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+		console.log("device ready");
+    }
+	
+
+
+
